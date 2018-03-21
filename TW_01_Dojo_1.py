4d2dd73c0032cc1477598a1f4d1bf65c3891e986
HardRic501 <hardric501@gmail.com>
2018-03-21 10:07:34 +0100

    Request counter - pro

--- TW_01_Dojo_1.py
+++ TW_01_Dojo_1.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
+counts = {}
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    global counts
+    return str(counts)
+
+
+@app.route('/request-counter', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def request_counter():
+    global counts
+    if request.method in counts:
+        counts[request.method] += 1
+    else:
+        counts[request.method] = 0
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
